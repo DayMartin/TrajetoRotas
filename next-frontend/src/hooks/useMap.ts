68ca44ba884c10d6cf0bf82451e3035ccb21e850
DayMartin <negociosdinah@gmail.com>
2024-12-13 19:32:48 -0300

    feat: rota do drive e rota do admin

--- next-frontend/src/hooks/useMap.ts
+++ next-frontend/src/hooks/useMap.ts
@@ -9,7 +9,7 @@ export function useMap(containerRef: React.RefObject<HTMLDivElement>) {
   useEffect(() => {
     (async () => {
       const loader = new Loader({
-        apiKey: "AIzaSyA6FS7NUcFZoDJum2gnrphB-7siPlnsIf8" as string,
+        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
         libraries: ["routes", "geometry", "marker"],
       });
       const [, , , position] = await Promise.all([
