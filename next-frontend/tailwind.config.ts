6a9cd28291f9246acbcf2cab82d25a15e104b02a
DayMartin <negociosdinah@gmail.com>
2024-12-13 11:26:48 -0300

    feat: iniciando frontend e buscando rotas do back

---
+++ next-frontend/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        default: "#242526",
+        main: "#ffcd00"
+      },
+      textColor: {
+        primary: "#242526",
+        contrast: "#ffffff"
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
