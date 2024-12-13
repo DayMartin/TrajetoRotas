aacdb4511cf3d16fbcf4721c7a91d8db05f53027
DayMartin <negociosdinah@gmail.com>
2024-12-13 02:22:16 -0300

    feat: api para conexçao com google maps

--- nestjs-api/src/maps/places/places.service.ts
+++ nestjs-api/src/maps/places/places.service.ts
@@ -1,55 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosResponse } from 'axios';
+import {
+  Client as GoogleMapsClient,
+  PlaceInputType,
+} from '@googlemaps/google-maps-services-js';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PlacesService {
-  private readonly nominatimUrl = 'https://nominatim.openstreetmap.org/search';
+  constructor(
+    private googleMapsCliente: GoogleMapsClient,
+    private configService: ConfigService,
+  ) {}
 
   async findPlaces(text: string): Promise<any> {
-    const response: AxiosResponse<any> = await axios.get(this.nominatimUrl, {
+    const { data } = await this.googleMapsCliente.findPlaceFromText({
       params: {
-        q: text,
-        format: 'json',
-        addressdetails: 1,
-        limit: 5,
+        input: text,
+        inputtype: PlaceInputType.textQuery,
+        fields: ['place_id', 'formatted_address', 'geometry', 'name'],
+        key: this.configService.get('GOOGLE_MAPS_API_KEY'),
       },
     });
 
-    const { data, statusText } = response;
-
-    let latitude_sul;
-    let latitude_norte;
-    let longitude_oeste;
-    let longitude_leste;
-
-    const boundingBoxs = data[0]?.boundingbox;
-    if (Array.isArray(boundingBoxs)) {
-      [latitude_sul, latitude_norte, longitude_oeste, longitude_leste] = boundingBoxs;
-    }
-
-    return {
-      statusText,
-      candidates: data.map((item) => ({
-        formatted_address: `${item.address.road}, ${item.address?.municipality} - ${item.address?.state}, ${item.address?.postcode}, ${item.address?.country}`,
-        geometry: {
-          location: {
-            lat: parseFloat(item.lat),
-            lgn: parseFloat(item.lon),
-          },
-        },
-        viewport: {
-          northeast: {
-            lat: parseFloat(latitude_norte),
-            lgn: parseFloat(longitude_leste),
-          },
-          southwest: {
-            lat: parseFloat(latitude_sul),
-            lgn: parseFloat(longitude_oeste),
-          },
-        },
-        name: item.display_name.split(',')[0],
-        place_id: item.place_id,
-      })),
-    };
+    return data;
   }
 }
